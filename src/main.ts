44a4f806514b0b4309e3a74692b6774316437c2c
AnandhuNu10 <anandhua@nu10.co>
2023-10-31 16:13:56 +0530

    updated commit

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [ 
-      'http://localhost:3000',
-      
+      // 'http://localhost:3000',
+      'https://todo-list-assignment-fe.vercel.app/'      
     ],
     methods: ["GET", "POST","PUT","DELETE","PATCH"],
     credentials: true,
