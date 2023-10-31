cc912580cbbf9cc243112fdfc91a809aaaf62d39
AnandhuNu10 <anandhua@nu10.co>
2023-10-31 15:19:38 +0530

    Deployment ongoing

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [ 
-      'http://localhost:3000',
-      'https://todo-list-assignment-fe-rveh.vercel.app/'
+      // 'http://localhost:3000',
+      "https://todo-list-assignment-fe.vercel.app"
+      
     ],
     methods: ["GET", "POST","PUT","DELETE","PATCH"],
     credentials: true,
